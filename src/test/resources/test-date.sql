cdbe7283c6e948a8aed119da0877bdba47daae68
Matt Egbers <matt.egbers@contractor.cengage.com>
2014-03-09 13:30:15 -0400

    Adding the test resources and sql

---
+++ src/test/resources/test-date.sql
@@ -0,0 +1,9 @@
+
+
+INSERT INTO landlord.user (id, email, password) VALUES (1001, 'a', 'a'); 
+INSERT INTO landlord.user (id, email, password) VALUES (1002, 'b', 'b'); 
+INSERT INTO landlord.user (id, email, password) VALUES (1003, 'c', 'c'); 
+INSERT INTO landlord.user (id, email, password) VALUES (1004, 'd', 'd'); 
+INSERT INTO landlord.user (id, email, password) VALUES (1005, 'e', 'e'); 
+INSERT INTO landlord.user (id, email, password) VALUES (1006, 'f', 'f'); 
+INSERT INTO landlord.user (id, email, password) VALUES (1007, 'g', 'g'); 
